620a8a555bb645a7624276cba1dbfffececc3ba0
Diveturtle93 <Hafemeister1@hotmail.de>
2025-08-21 04:17:18 +0200

    Readme, Kommentare

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# STM32_Resetreason
+# Resetreason
+
+In dieser Bibliothek wird der Grund für einen Reset des Systems ausgewertet. Die Funktion
+schaut sich das entsprechende Register an und gibt dann zurück warum der µC zurückgesetzt
+wurde.
--- ResetReason.c
+++ ResetReason.c
@@ -104,22 +104,26 @@ void printResetSource(reset_reason reset_flags)
 	}
 	else
 	{
+		// Pruefe Reset Flag Internen Watchdog
 		if (reset_flags & IWDGRST1)											// Independent watchdog Reset
 		{
 			uartTransmit("Interner Watchdog Reset\n", 24);
 		}
-
+		
+		// Pruefe Reset Flag Window Watchdog
 		if (reset_flags & WWDGRST1)											// Window watchdog Reset
 		{
 			uartTransmit("Window Watchdog Reset\n", 22);
 		}
-
+		
+		// Pruefe Reset Flag Low Power Reset
 		if (reset_flags & LPWRRST1)											// Low Power Reset
 		{
 			uartTransmit("Low Power Reset\n", 16);
 		}
 	
 #ifdef STM32F7
+		// Pruefe Reset Flag Brown Out Reset
 		if (reset_flags & BORRST1)											// Brown out Reset
 		{
 			uartTransmit("Brown Out Reset\n", 16);
@@ -127,22 +131,26 @@ void printResetSource(reset_reason reset_flags)
 #endif
 
 #ifdef STM32G0
+		// Pruefe Reset Flag Option Byte Loader Reset
 		if (reset_flags & OBLRST1)											// Option Byte Loader Reset
 		{
 			uartTransmit("Option Byte Loader Reset\n", 25);
 		}
 #endif
 
+		// Pruefe Reset Flag Power On Reset
 		if (reset_flags & PORRST1)											// Power on Reset / Power down Reser
 		{
 			uartTransmit("Power On Reset\n", 15);
 		}
-
+		
+		// Pruefe Reset Flag Software Reset
 		if (reset_flags & SFTRST1)											// Software Reset
 		{
 			uartTransmit("Software Reset\n", 15);
 		}
-
+		
+		// Pruefe Reset Flag Pin-Reset
 		if (reset_flags & PINRST1)											// NRST pin
 		{
 			uartTransmit("PIN Reset\n", 10);
